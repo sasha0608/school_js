3ac523d17930a12278b441c541fa7c7f6caee7a7
sasha <shynkarenko0608@gmail.com>
2023-03-16 11:08:00 +0200

    /

---
+++ js.js
@@ -0,0 +1,16 @@
+// 1
+// let a;
+// a = 10;
+// console.log(a)
+// a = 20;
+// console.log(a)
+
+// 2
+// let num = '123';
+// let sum = Number(num [0]) + Number(num [1]) + Number(num [2]);
+// console.log(sum);
+
+//3
+// let w = prompt("ведіть ширину прям");
+// let h = prompt("ведіть висоту прям");
+// console.log(w*h);
